7e4008a8638bb76f936bcff1700e70177852f144
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2025-05-29 07:23:12 +0900

    Add initial project files for gitSyncApp including solution and project configurations

---
+++ src/gitSyncApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace gitSyncApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}");
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
